5f8df79b6b78366071a71e4de21c171d61c75fa1
Zig Zichterman <github@ziggr.com>
2019-11-29 15:35:41 -0800

    one line per character
    2-2 cells per crafting type (3 for smithing, 2 for JW+consumable). Fixed column positions should be table-able.

--- tools/explore.lua
+++ tools/explore.lua
@@ -13,35 +13,33 @@ CHAR_ABBREV = {
     ["Zhaksyr the Mighty"] = "zh"
 }
 
-CRAFT_TYPE = {
-    ["ALCHEMY"     ] = 4
-,   ["ENCHANTING"  ] = 3
-,   ["PROVISIONING"] = 5
+CRAFT = {
+    ["BLACKSMITHING"] = { ord = 1, type = 1, abbrev = "bs", req_ct = 3 }
+,   ["CLOTHIER"     ] = { ord = 2, type = 2, abbrev = "cl", req_ct = 3 }
+,   ["WOODWORKING"  ] = { ord = 3, type = 6, abbrev = "ww", req_ct = 3 }
+,   ["JEWELRY"      ] = { ord = 4, type = 7, abbrev = "jw", req_ct = 2 }
+,   ["ALCHEMY"      ] = { ord = 5, type = 4, abbrev = "al", req_ct = 2 }
+,   ["ENCHANTING"   ] = { ord = 6, type = 3, abbrev = "en", req_ct = 2 }
+,   ["PROVISIONING" ] = { ord = 7, type = 5, abbrev = "pr", req_ct = 2 }
 }
-
-CRAFT_ABBREV = {
-    [CRAFT_TYPE.ALCHEMY     ] = "al"
-,   [CRAFT_TYPE.ENCHANTING  ] = "en"
-,   [CRAFT_TYPE.PROVISIONING] = "pr"
-}
-
-CRAFT_REQ_CT = {
-    [CRAFT_TYPE.ALCHEMY     ] = 2
-,   [CRAFT_TYPE.ENCHANTING  ] = 2
-,   [CRAFT_TYPE.PROVISIONING] = 2
-}
+                        -- Index craft by numeric CRAFTING_TYPE_X
+                        -- in addition to above index by symbol.
+local cc = {}
+for k,v in pairs(CRAFT) do cc[v.type] = v end
+for k,v in ipairs(cc) do CRAFT[k] = v end
+cc = nil
 
 REQUIREMENT_ABBREV = {
-    [CRAFT_TYPE.ALCHEMY] = {
+    [CRAFT.ALCHEMY.type] = {
         ["Drain Health Poison"] = "xdrh"
     ,   ["Lorkhan's Tears"    ] = "lork"
     ,   ["Alkahest"           ] = "alka"
     }
-,   [CRAFT_TYPE.ENCHANTING] = {
+,   [CRAFT.ENCHANTING.type] = {
         ["Glyph of Stamina"   ] = "stam"
     ,   ["Ta Aspect Rune"     ] = "ta"
     }
-,   [CRAFT_TYPE.PROVISIONING] = {
+,   [CRAFT.PROVISIONING.type] = {
         ["Firsthold Fruit and Cheese Plate"] = "ffcp"
     ,   ["Muthsera's Remorse"              ] = "muth"
     }
@@ -73,16 +71,24 @@ local function Error(...)
     print(string.format(...))
 end
 
+local function rdump(t)
+    if type(t) ~= "table" then return tostring(t) end
+
+    local s = "{"
+    for k,v in pairs(t) do s = s..k..":"..rdump(v).." " end
+    s = s.."}"
+    return s
+end
+
 local function dump(t)
-    local s = ""
-    for k,v in pairs(t) do s = s..k.." " end
-    print(s)
+    print(rdump(t))
 end
 
 function ToRowString(input_row)
     if (not input_row) or (input_row.quest_type ~= "daily") then
         return nil
     end
+    local craft = CRAFT[input_row.crafting_type]
 
     local req_abbrev    = REQUIREMENT_ABBREV[input_row.crafting_type]
     if not req_abbrev then
@@ -92,7 +98,7 @@ function ToRowString(input_row)
 
     local req_list = AbbreviateMulti(req_abbrev, input_row.desc2)
                         -- Pad to a fixed number of reqs for each crafting type.
-    local req_ct   = CRAFT_REQ_CT[input_row.crafting_type]
+    local req_ct   = craft.req_ct
     for i = #req_list,req_ct-1 do
         table.insert(req_list,"")
     end
@@ -100,16 +106,79 @@ function ToRowString(input_row)
     local req      = table.concat(req_list,"\t")
     local char     = AbbreviateOne(CHAR_ABBREV, input_row.char_name)
     local date     = input_row.time:sub(1,10)
-    local ctype    = CRAFT_ABBREV[input_row.crafting_type]
+    local ctype    = craft.abbrev
 
     return string.format("%s\t%s\t%s\t%s", date, char, ctype, req)
 end
 
+function Accumulate(input_row, output_row)
+    if (not input_row) or (input_row.quest_type ~= "daily") then
+        return nil
+    end
+                        -- Did we move to a new character or date?
+                        -- Output current accumulator and start a
+                        -- fresh one.
+    local output   = nil
+    local date     = input_row.time:sub(1,10)
+    if (output_row.date      ~= date)
+            or (output_row.char_name ~= input_row.char_name ) then
+        output               = ToOutput(output_row)
+        output_row.date      = date
+        output_row.char_name = input_row.char_name
+        output_row.req       = {}
+        for ctype,c in ipairs(CRAFT) do
+            local r = {}
+            for i = 1,c.req_ct do
+                table.insert(r,"")
+            end
+            output_row.req[c.ord] = r
+        end
+    end
+
+    local crafting_type = input_row.crafting_type
+    local craft         = CRAFT[crafting_type]
+    local req_abbrev    = REQUIREMENT_ABBREV[input_row.crafting_type]
+    if not req_abbrev then
+        Error("unknown crafting_type:%d", input_row.crafting_type)
+        return output
+    end
+
+    local req_list = AbbreviateMulti(req_abbrev, input_row.desc2)
+                        -- Pad to a fixed number of reqs for each crafting type.
+    for i = #req_list,craft.req_ct-1 do
+        table.insert(req_list,"")
+    end
+    output_row.req[craft.ord] = req_list
+-- dump(output_row)
+    return output
+end
+
+function ToOutput(output_row)
+    local t = {}
+    if not output_row.date then return nil end
+    table.insert(t, output_row.date)
+    table.insert(t, output_row.char_name)
+    for ord = 1,7 do
+        local req = output_row.req[ord]
+        local s   = table.concat(req, "\t")
+        table.insert(t, s)
+    end
+    return table.concat(t, "\t")
+end
+
+local output_row = {}
 for _,input_row in ipairs(input) do
-    local line = ToRowString(input_row)
+    local line = Accumulate(input_row, output_row)
+-- dump(output_row)
     if line then
         print(line)
     end
 end
+-- dump(output_row)
+local last_line = ToOutput(output_row)
+if last_line then
+    print(last_line)
+end
+
 
 
